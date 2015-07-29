de958cb143b9ad322de4b086ba599a5bd748765c
Vanya <19kerer97@mail.ru>
2015-07-30 01:56:01 +0300

    Figure created

---
+++ tst/oi/hexled/xo/PlayerTest.java
@@ -0,0 +1,21 @@
+package oi.hexled.xo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vanya on 30.07.2015.
+ */
+public class PlayerTest {
+
+    @Test
+    public void testGetFiger() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+}
